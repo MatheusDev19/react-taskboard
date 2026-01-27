d197d11d9cfe4942c106cd2ff0f29e9a99fa086a
matheus-rodrigues <matheusolvr.dev@gmail.com>
2026-01-27 11:41:53 -0300

    feat: mover o componente AppRouter para um novo arquivo e corrigir importação

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "./styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AppRouter } from "./routes/routes.tsx";
+import { AppRouter } from "./routes.tsx";
 import { GlobalProvider } from "./GlobalProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "../layout";
+import RootLayout from "./layout";
 
 const router = createBrowserRouter([
   {
