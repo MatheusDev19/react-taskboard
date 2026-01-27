597cbda6b444d586ff4c30ba2abee6f2923b3291
matheus-rodrigues <matheusolvr.dev@gmail.com>
2026-01-26 22:21:11 -0300

    feat: atualizar estrutura do layout, adicionando cabeçalho e ajustando navbar

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,11 +4,12 @@ import AppShell from "./components/AppShell";
 export default function RootLayout() {
   return (
     <AppShell>
-      <AppShell.Navbar>{/* <NavbarActions /> */}</AppShell.Navbar>
+      <AppShell.Header>Header</AppShell.Header>
+      <AppShell.Navbar>NavbarActions{/* <NavbarActions /> */}</AppShell.Navbar>
       <AppShell.Main>
         <Outlet />
       </AppShell.Main>
-      <AppShell.Footer>{/* <FooterActions /> */}</AppShell.Footer>
+      <AppShell.Footer>FooterActions{/* <FooterActions /> */}</AppShell.Footer>
     </AppShell>
   );
 }
